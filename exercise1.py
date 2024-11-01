88f8a94a5a6a2ee704df4c770beb48890b9e8629
Sneha Gaikwad <snehagaikwad0666@gmail.com>
2024-11-01 16:33:59 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+# Exercise 1 
+# Create a calculator capable of performing addition, subtraction , multiplication and division operations on two numbers . your program should format the output in a readable manner . 
+
+num1 = 15 ;
+num2 = 10 ;
+
+print("The addition of two numbers is",(num1+num2)) 
+print("The subtraction of two numbers is ",(num1-num2))
+print("The multiplication of two numbers is ",(num1*num2))
+print("The division of two numbers is ",(num1/num2))
